fff9ca49bd65da27605b808b88d8b7c5e67aec32
小海 <837233792@qq.com>
2019-02-13 13:11:23 +0800

    数据请求封装

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,18 +1,24 @@
-// pages/classic/classic.js
+import { ClassicModel } from '../../models/classic.js'
+let classic = new ClassicModel()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    classic: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    classic.getLatest((res) => {
+      this.setData({
+        classic: res
+      })
+    })
   },
 
   /**
