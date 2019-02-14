1557f7e2a57a30d4b4ef79ac581ab1124d12c4f3
小海 <837233792@qq.com>
2019-02-14 13:09:13 +0800

    promise以及底部状态栏

--- util/http-p.js
+++ util/http-p.js
@@ -7,28 +7,33 @@ const tips = {
 }
 
 class HTTP {
-  request (params) {
-    if (!params.method) {
-      params.method = "GET"
-    }
+  request ({url, data={}, method='GET'}) {
+    return new Promise((resolve, reject) => {
+      this._request(url, resolve, reject, data, method)
+    })
+  }
+
+  _request (url, resolve, reject, data={}, method='GET') {
     wx.request({
-      url: config.api_base_url + params.url,
-      method: params.method,
-      data: params.data,
+      url: config.api_base_url + url,
+      method: method,
+      data: data,
       header: {
         'content-type': 'application/json',
         'appkey': config.appkey
       },
       success: (res) => {
-        let code = res.statusCode.toString()
+        const code = res.statusCode.toString()
         if (code.startsWith('2')) {
-          params.success && params.success(res.data)
+          resolve(res.data)
         } else {
-          let error_code = res.data.error_code
+          reject()
+          const error_code = res.data.error_code
           this._show_error(error_code)
         }
       },
       fail: (err) => {
+        reject()
         this._show_error(1)
       }
     })
